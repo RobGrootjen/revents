648539f85708f58d9dfd211dd66d403595d4da73
Rob Grootjen <robgrootjen1@gmail.com>
2021-01-16 10:14:20 -0600

    New update

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventListItem from './EventListenItem';
+
+export default function EventList() {
+    return(
+        <>
+          <EventListItem />
+          <EventListItem />
+          <EventListItem />
+          <EventListItem />
+        </>
+    )
+}
--- src/features/events/eventDashboard/eventDashboard.jsx
+++ src/features/events/eventDashboard/eventDashboard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import EventList from './EventList';
 
 export default function EventDashboard(){
     return (
         <Grid>
             <Grid.Column width={10}>
-                <h2>Left Column</h2>
+                <EventList />
             </Grid.Column>
             <Grid.Column width={6}>
                 <h2>Right Column</h2>
             </Grid.Column>
+
+
         </Grid>
     )
 }
